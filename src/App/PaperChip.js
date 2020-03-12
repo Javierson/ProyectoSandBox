716fa9a0a45d7c130bce99f9bba6d9e403acbdf3
Javierson <warrior_alfa92@hotmail.com>
2020-03-12 16:22:44 -0700

    Arreglo del paper chip

--- src/App/PaperChip.js
+++ src/App/PaperChip.js
@@ -2,16 +2,12 @@ import React from "react";
 import { Paper, Grid, Typography } from "@material-ui/core";
 
 const Message = (X, Y) => {
-  if (!Y || Y === 0 )
-  return <Typography align="center">{X}</Typography>
-},
-PaperChip = ({ XS = 12, Title, Length, children }) => (
-  <Grid item xs={XS}>
-    { children }
-    <Paper>
-      { Message(Title, Length) }
-    </Paper>
-  </Grid>
-);
+    if (!Y || Y === 0) return <Typography align="center">{X}</Typography>;
+  },
+  PaperChip = ({ XS = 12, Title, Length, children }) => (
+    <Grid item xs={XS}>
+      <Paper>{children && Message(Title, Length)}</Paper>
+    </Grid>
+  );
 
 export default PaperChip;
