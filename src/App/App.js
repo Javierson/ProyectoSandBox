d257c1f73cc48552272ea84c18a0fc5774564afb
Javierson <warrior_alfa92@hotmail.com>
2020-03-12 13:26:54 -0700

    App

--- src/App/App.js
+++ src/App/App.js
@@ -5,12 +5,22 @@ import { Alert, Drawer } from "."
 import { Container } from "@material-ui/core"
 import { Route, Switch } from "react-router-dom"
 
-const { Routes = [ ] } = require("../AppModules"),
+import { FormEmployeed } from "../Employeed";
+import { FormCreateTool } from "../Tool";
+import { FormCreateCenter } from "../Center";
+
+
+const // { Routes = [ ] } = require("../AppModules"),
+Test = () => <h2>Test</h2>,
 AlertMessage = () => <Alert Severity="info">Ruta no definida</Alert>,
 App = () => <Container>
       <Drawer>
         <Switch>
-          { /* Routes.map( ({ Path, Component }, _) => <Route key = { _ } exact path = { Path } component = { Component }/> ) */ }
+        {[{ Path: "/", Component: <Test /> },
+        { Path: "/Registrar usuario", Component: <FormEmployeed /> },
+        { Path: "/Registrar herramienta", Component: <FormCreateTool /> },
+        { Path: "/Registrar centro", Component: <FormCreateCenter /> }].map( ({ Path, Component }, _) => <Route key = { _ } exact path = { Path } component = { Component }/> )}
+        { /* Routes.map( ({ Path, Component }, _) => <Route key = { _ } exact path = { Path } component = { Component }/> ) */ }
           <Route component = { AlertMessage }/>
         </Switch>
       </Drawer>
